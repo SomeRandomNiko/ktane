30c7c65d49a97191bd3855821c32e2f15eef9613
SomeRandomNiko <SomeRandomNiko@OMEN-NIKO.fritz.box>
2020-02-22 12:15:12 +0100

    22.02.2020
    Keypad is now fully functional

--- KeepTalkingAndNobodyExplodes/src/modules/keypad/Keypad.java
+++ KeepTalkingAndNobodyExplodes/src/modules/keypad/Keypad.java
@@ -1,5 +1,6 @@
 package modules.keypad;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -8,14 +9,17 @@ import java.util.Random;
 
 import javax.imageio.ImageIO;
 
+import bomb.Bomb;
 import modules.Hitbox;
 import modules.Module;
 
 public class Keypad extends Module {
 
 	ArrayList<Integer> symbolList = new ArrayList<>();
+	ArrayList<Integer> inter = new ArrayList<>();
 	private Key[] keys;
 	private int row;
+	private int order = 0;
 	private BufferedImage keypad;
 	private final int[][] symbols = {
 			{
@@ -33,7 +37,6 @@ public class Keypad extends Module {
 
 	public Keypad(int moduleIndex) {
 		super(moduleIndex);
-
 		try {
 			keypad = ImageIO.read(getClass().getResourceAsStream("/modules/keypad/keypad.png"));
 		} catch (IOException e) {
@@ -47,18 +50,47 @@ public class Keypad extends Module {
 			if (!symbolList.contains(symbols[row][symbol])) {
 				symbolList.add(symbols[row][symbol]);
 				keys[i] = new Key(i, symbolList.get(i));
-				hitboxes[i] = new Hitbox(getModuleOffset()[0] + (i < 2 ? i : i - 2) * (174 + 16) + 61, getModuleOffset()[1] + (i < 2 ? 0 : 1) * (174 + 16) + 83, 174, 174);
+				hitboxes[i] = new Hitbox(getModuleOffset()[0] + 50 + (i < 2 ? i : i - 2) * (160 + 40), getModuleOffset()[1] + 68 + (i < 2 ? 0 : 1) * (160 + 40), 183, 183);
 			} else {
 				i--;
 			}
 		}
+		for (int x : symbols[row])
+			if (symbolList.contains(x))
+				inter.add(x);
 		setHitboxes(hitboxes);
 	}
 
 	public void update(Graphics g) {
 		g.drawImage(keypad, getModuleOffset()[0], getModuleOffset()[1], null);
 		for (int i = 0; i < keys.length; i++) {
-			g.drawImage(keys[i].getSymbol(), getModuleOffset()[0] + 62 + (i < 2 ? i : i - 2) * (160 + 40), getModuleOffset()[1] + (i < 2 ? 0 : 1) * (160 + 40) + 90, null);
+			g.drawImage(keys[i].getSymbol(), getModuleOffset()[0] + 62 + (i < 2 ? i : i - 2) * (160 + 40), getModuleOffset()[1] + 90 + (i < 2 ? 0 : 1) * (160 + 40), null);
+			
+			if(keys[i].isPressed()) {
+				g.setColor(new Color(0x06c000));
+				g.fillRect(getModuleOffset()[0] + 126 + (i < 2 ? i : i - 2) * 200, getModuleOffset()[1] + 85 + (i < 2 ? 0 : 1) * 200, 32, 14);
+			}
+			
+			if (getHitboxes()[i].isClick() && !keys[i].isPressed()) {
+				if (isInOrder(keys[i].getSymbolNumber(), order)) {
+					keys[i].setPressed(true);
+					order++;
+				} else {
+					Bomb.setExplode(true);
+				}
+			}
+			
+			if(order >= 4) {
+				setSolved(true);
+			}
 		}
+	}
+
+	private boolean isInOrder(int symbolNumber, int order) {
+		boolean ret = false;
+		if (inter.get(order) == symbolNumber) {
+			ret = true;
+		}
+		return ret;
 	}
 }
